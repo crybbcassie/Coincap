6f0a6db3285e48951af3d794694e8e8dad863f7c
Cassie <crybbcassie@gmail.com>
2023-07-24 14:44:00 +0300

    fixl latest transaction info

--- src/components/modals/WalletModal.jsx
+++ src/components/modals/WalletModal.jsx
@@ -5,12 +5,21 @@ import { useState, useEffect } from "react";
 
 export default function WalletModal({ open, onCancel, onTotalSumChange }) {
   const [total, setTotal] = useState(0);
+  const [previousTotal, setPreviousTotal] = useState(0);
 
   const handleTotalChange = (value) => {
+    setPreviousTotal(total);
     setTotal(value);
   };
 
-  const totalSum = `${Math.round(total * 1000) / 1000}`;
+  function slash(n){
+    return `${Math.round(n * 1000) / 1000}`;
+  }
+
+  const totalSum = slash(total);
+  const difResult = slash(total - previousTotal) 
+  const difference = difResult > 0? '+' + difResult : difResult;
+  const percentageChange = slash((total - previousTotal / previousTotal) * 100);
 
   useEffect(() => {
     onTotalSumChange(totalSum);
@@ -25,15 +34,16 @@ export default function WalletModal({ open, onCancel, onTotalSumChange }) {
       width={700}
     >
       <div className={classes.walletContent}>
-            <WalletTable onTotalChange={handleTotalChange} />
-            <div className={classes.calc}>
-              <div className={classes.total}>
-                Total sum: <span>${totalSum} </span>
-              </div>
-              <div className={classes.transaction}>
-                Latest transaction: <span>+2.30</span> (1.8%)
-              </div>
-            </div>
+        <WalletTable onTotalChange={handleTotalChange} />
+        <div className={classes.calc}>
+          <div className={classes.total}>
+            Total sum: <span>${totalSum} </span>
+          </div>
+          <div className={classes.transaction}>
+            Latest transaction: <span>{difference}$</span> ({percentageChange}
+            %)
+          </div>
+        </div>
       </div>
     </Modal>
   );
